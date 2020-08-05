27f64bde738e327d14c863a3223e52670dbafe4a
Haziq Hapiz <zyqq.dev@gmail.com>
2020-08-05 21:51:59 +0800

    react test

---
+++ components/counter.jsx
@@ -0,0 +1,55 @@
+//1:39:19
+//1;55;42
+
+import React, { Component } from "react";
+
+class Counter extends Component {
+  // state = { value: this.props.counter.value };
+
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
+
+  render() {
+    console.log("Counter - Rendered");
+    return (
+      <div>
+        {this.props.children}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-seondary btn-sm"
+        >
+          {" "}
+          Increment{" "}
+        </button>
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className="btn btn-seondary btn-sm"
+        >
+          {" "}
+          Decrement{" "}
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.props.counter;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
---
+++ components/counters.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    console.log("Counters - Rendered");
+    const {
+      onReset,
+      counters,
+      onDelete,
+      onIncrement,
+      onDecrement,
+      onAdd,
+    } = this.props;
+
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        <button onClick={onAdd} className="btn btn-primary btn-sm m-2">
+          Add
+        </button>
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onAdd={onAdd}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            counter={counter}
+          >
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
